5355908fbd574addc2b19cc4aeeeb167828dbce9
Alex <17277800+alex-schaaf@users.noreply.github.com>
2024-12-07 12:04:47 +0100

    add solution for day 7.2

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 2. [Solution](https://github.com/alex-schaaf/adventofcode2024/blob/main/day02/src/main.rs). This one made me learn about cloning vectors, how to turn vectors into iterators, and anonymous functions. I got a bit stuck at first, as I was including 0 as an acceptable value when checking for the gradient monotony. Luckily writing some tests for my gradient monotony function got me to figure that out quickly!
     - Learnt about the `Vec::windows` method, that returns an iterator over a window of given size. So you can just `.map` over it using an anonymous diff function to calculate the gradient array.
 3. [Solution](https://github.com/alex-schaaf/adventofcode2024/blob/main/day03/src/main.rs). Simple regex parsing. This time I learned more about how to implement functions that return an `Option<T>`. Combined with the `?` shorthand for propagating errors, this makes for really nice happy-path coding without the verbosity that you see in Go code.
+4.
+5.
+6.
+7. [Solution](https://github.com/alex-schaaf/adventofcode2024/blob/main/day07/src/main.rs). I got a bit annoyed here with Rust, as creating the cartesian product of operators felt like an unneccessary hassle to get Rust to do what I wanted it to do. A stark contrast to Python's `itertools`. Also first time in my life I had to deal with an integer overflow, because I was using first `i32` for the calculations, but it required `i64`. Thankfully Rust just tells you that when trying to parse a stringified number.
--- day07/src/main.rs
+++ day07/src/main.rs
@@ -7,11 +7,16 @@ fn main() {
 
     let result1 = puzzle1(&input);
     println!("Puzzle 1: {}", result1);
+
+    let result2 = puzzle2(&input);
+    println!("Puzzle 2: {}", result2);
 }
+
 #[derive(Clone, Debug)]
 enum Operator {
-   Add,
-   Multiply 
+    Add,
+    Multiply,
+    Concat,
 }
 
 fn puzzle1(input: &str) -> i64 {
@@ -21,12 +26,12 @@ fn puzzle1(input: &str) -> i64 {
 
     let mut sum: i64 = 0;
     for (test_number, operands) in equations {
-        let operator_combinations = (0..operands.len()-1).map(|_| allowed_operators.clone()).multi_cartesian_product().collect_vec();
+        let operator_combinations = get_combinations(&allowed_operators, operands.len() - 1);
         for operators in operator_combinations {
             let result = parse_expression(&operands, operators);
             if result == test_number {
                 sum += test_number;
-                break
+                break;
             }
         }
     }
@@ -34,11 +39,40 @@ fn puzzle1(input: &str) -> i64 {
     sum
 }
 
+fn puzzle2(input: &str) -> i64 {
+    let equations = parse(input);
+
+    let allowed_operators = vec![Operator::Add, Operator::Multiply, Operator::Concat];
+
+    let mut sum: i64 = 0;
+    for (test_number, operands) in equations {
+        let operator_combinations = get_combinations(&allowed_operators, operands.len() - 1);
+        for operators in operator_combinations {
+            let result = parse_expression(&operands, operators);
+            if result == test_number {
+                sum += test_number;
+                break;
+            }
+        }
+    }
+
+    sum
+}
+
+fn get_combinations<T: Clone>(values: &Vec<T>, n: usize) -> Vec<Vec<T>> {
+    (0..n)
+        .map(|_| values.clone())
+        .multi_cartesian_product()
+        .collect_vec()
+}
+
 fn parse(input: &str) -> Vec<(i64, Vec<i64>)> {
     let mut equations: Vec<(i64, Vec<i64>)> = Vec::new();
 
     for line in input.lines() {
-        let (test_number_str, operands_str) = line.split_once(": ").expect("there to be a test value and operation numbers");
+        let (test_number_str, operands_str) = line
+            .split_once(": ")
+            .expect("there to be a test value and operation numbers");
         let test_number: i64 = test_number_str.parse().expect("failed to parse str to i64");
         let mut operands: Vec<i64> = Vec::new();
         for operand_str in operands_str.split_whitespace() {
@@ -56,6 +90,11 @@ fn parse_expression(operands: &Vec<i64>, operators: Vec<Operator>) -> i64 {
         match operator {
             Operator::Add => previous_value += operand,
             Operator::Multiply => previous_value *= operand,
+            Operator::Concat => {
+                previous_value = (previous_value.to_string() + &operand.to_string())
+                    .parse()
+                    .expect("failed to parse concatenated string to i64")
+            }
         }
     }
     previous_value
